5f32e20c02e092d19ae9062fdae26b2fff8eb2aa
emaanbilal <emaanbilal@gmail.com>
2025-10-15 17:31:34 -0400

    Remove .vercel and .svelte-kit from git tracking, update .gitignore

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.C4dRiPld.js","_app/immutable/chunks/scheduler.B2QSKqcC.js","_app/immutable/chunks/index.BRMTjanE.js"];
-export const stylesheets = ["_app/immutable/assets/2.E3IkhWRk.css"];
-export const fonts = [];
